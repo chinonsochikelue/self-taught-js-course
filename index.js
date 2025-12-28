95d11315c47b5d048da559cc4e506bede34a3407
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2025-12-28 09:51:51 +0100

    Add loop challenge to print multiples of 3 and 5

--- index.js
+++ index.js
@@ -22,3 +22,52 @@ const compareNumbers = (a, b) => {
         return "Same"
     }
 }
+
+
+
+
+// LOOPS (Pattern Thinking)
+// 🎯 Goal
+
+// Teach your brain to repeat logic without repeating code.
+
+// CHALLENGE
+
+// Write a program that:
+
+// Loops from 1 to 50
+
+// Prints:
+
+// "Code" for multiples of 3
+
+// "Logic" for multiples of 5
+
+// "CodeLogic" for multiples of both
+
+// The number itself otherwise
+
+// 📌 Rules
+
+// Use a for loop
+
+// Use if / else if / else
+
+// No arrays
+
+// No shortcuts
+
+
+
+
+for (let i =1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("CodeLogic")
+    } else if (i % 3 === 0) {
+        console.log("Code")
+     } else if ( i % 5 === 0 ) {
+        console.log("Logic")
+    } else {
+        console.log(i);
+    }
+}
