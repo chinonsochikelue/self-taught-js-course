2f86fa1e5608c0623fbcd8ce8f9fb6c6974a6a40
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2025-12-28 09:37:54 +0100

    Add comments to clarify the purpose and functionality of the compareNumbers function

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
 
 // Teach your brain to write reusable logic instead of hard-coding values.
 
+// Write a function that:
+
+// Accepts two numbers
+
+// Returns:
+
+// The larger number
+
+// If both numbers are equal, return "Same"
+
 const compareNumbers = (a, b) => {
     if (a > b) {
         return a
