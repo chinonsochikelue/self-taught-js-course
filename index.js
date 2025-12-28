1ace7cb84ae881547ea928909272f53c30a30af0
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2025-12-28 09:36:04 +0100

    Add initial HTML and JavaScript files with basic structure and comparison function

---
+++ index.js
@@ -0,0 +1,14 @@
+// FUNCTIONS WITH PARAMETERS & RETURN
+// 🎯 Goal
+
+// Teach your brain to write reusable logic instead of hard-coding values.
+
+const compareNumbers = (a, b) => {
+    if (a > b) {
+        return a
+    } else if (b > a) {
+        return b
+    } else {
+        return "Same"
+    }
+}
