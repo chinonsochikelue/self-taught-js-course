45e3589c04a4c2476322f5f000bfc117aa4c53da
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2026-01-01 21:44:12 +0100

    Refactor switch statements for better readability and add Rock Paper Scissors game logic

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ let randomNum = Math.floor(Math.random() * 6);
 let userQuestion = prompt("Ask me anything: ");
 let response = "";
 
-switch((randomNum)) {
+switch ((randomNum)) {
     case 0:
         response = "Hello there!";
         break;
@@ -277,7 +277,7 @@ switch((randomNum)) {
     case 5:
         response = "Too low! Try again.";
         break;
-    };
+};
 
 console.log(`Your question: ${userQuestion}`);
 console.log(`Answer: ${response}`);
@@ -299,7 +299,7 @@ let prize = parseInt(prompt("Select a number from 0 - 10: "));
 
 let message = "";
 
-switch(prize) {
+switch (prize) {
     case 0:
     case 1:
     case 2:
@@ -324,3 +324,54 @@ switch(prize) {
 };
 
 console.log("My Selection: ", message);
+
+
+// Rock Paper Scissors game
+// This is a game between a player and the computer, where both will make a random
+// selection of either Rock, Paper, or Scissors (alternatively, you could create a version
+// using real player input!). Rock will beat out Scissors, Paper will beat out Rock, and
+// Scissors will beat out Paper. You can use JavaScript to create your own version of
+// this game, applying the logic with an if statement. Since this project is a little more
+// difficult, here are some suggested steps:
+// 1. Create an array that contains the variables Rock, Paper, and Scissors.
+// 2. Set up a variable that generates a random number 0-2 for the player and then
+// do the same for the computer's selection. The number represents the index
+// values in the array of the 3 items.
+// 3. Create a variable to hold a response message to the user. This can show the
+// random results for the player and then also the result for the computer of the
+// matching item from the array.
+// 4. Create a condition to handle the player and computer selections. If both are
+// the same, this results in a tie.
+// 5. Use conditions to apply the game logic and return the correct results.
+// There are several ways to do this with the condition statements, but you
+// could check which player's index value is bigger and assign the victory
+// accordingly, with the exception of Rock beating Scissors.
+// 6. Add a new output message that shows the player selection versus the
+// computer selection and the result of the game.
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+let randomNumForUser = Math.floor(Math.random() * 3);
+let userChoice = choices[randomNumForUser];
+
+let randomNumForComputer = Math.floor(Math.random() * 3);
+let computerChoice = choices[randomNumForComputer];
+
+let responseMessage = "";
+
+if (userChoice === computerChoice) {
+    responseMessage = "It\'s a tie!";
+} else if (
+    userChoice === "Rock" && computerChoice === "Scissors" ||
+    userChoice === "Paper" && computerChoice === "Rock" ||
+    userChoice === "Scissors" && computerChoice === "Paper"
+) {
+    responseMessage = "You win!";
+} else {
+    responseMessage = "Computer win!";
+}
+
+
+console.log("User Selection: ", userChoice);
+console.log("Computer Selection: ", computerChoice);
+console.log(responseMessage);
